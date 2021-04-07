a28dac6878906d09db24649e179b029f62d00981
anjar fitria <81375454+anjarfitria@users.noreply.github.com>
2021-04-06 22:41:17 -0700

    Tugas minggu 1-4

---
+++ Tugas minggu 1-4/resources/views/gambar.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> gambar </title>
+    <style>
+      html, body {
+        background-color: #808000;
+        color: #00FFFF;
+        font-family: 'Nunito', sans-serif;
+        font-weight: 100;
+        height: 100vh;
+        margin: 0;
+      }
+      .full-height {
+        height: 100vh;
+      }
+      .flex-center {
+        align-items: baseline;
+        display: flex;
+        justify-content: center;
+      }
+      .position-ref {
+        position: relative;
+      }
+      .top-right {
+        position: absolute;
+        right: 10px;
+        top: 18px;
+      }
+      .content {
+        text-align: center;
+      }
+      img {
+        max-width: 50%;
+        height: auto;
+      }
+      .m-b-md {
+        margin-bottom: 20px;
+      }
+      .title {
+        border-top: 5px solid;
+        font-size: 20px;
+        font-weight: bold;
+        text-transform: uppercase;
+        border-bottom: 5px solid;
+        padding: 20px 0;
+        color: #964B00;
+      }
+    </style>
+  </head>
+
+  <body>
+    <div class="flex-center position-ref full-height">
+      <img src="/img/gmbar 3.jpg" alt="gmbar 3.jpg" width="200" height="auto">
+      <div class="content">
+        <img src="/img/gambar 1.jpg" alt="gambar 1" width="300" height="auto">
+        <div class="title m-b-md">
+        makanan favorite saya 
+        </div>
+        <img src="/img/gambar 2.jpg" alt="gambar 2.jpg" width="300" height="auto">
+      </div>
+      <img src="/img/gambar 4.jpg" alt="gambar 4" width="300" height="auto"
+    </div>
+  </body>
+</html>
---
+++ Tugas minggu 1-4/routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use app\http\controllers\Anjarcontroller;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|*/
+
+Route::get('/', function () {
+  return view('welcome');
+});
+/*Route::get('/hello', function () {
+   return 'Hello World';
+}); 
+Route::get('/belajar', function () {
+  echo '<h1>Hello World</h1>';
+    echo '<p>Sedang belajar Laravel</p>';   
+});
+Route::get('page/{nomor}',function($nomor){ 
+  return 'ini Halaman ke-'.$nomor;   
+});
+Route::get('/gambar', function () {
+  return view('gambar');
+});*/
+
+Route::get('user',[Anjarcontroller::class, '@index']);
+
+Route::resource('user', Anjarcontroller::class);
+
+Route::get("/watermelon", function(){
+  return view("watermelon");
+});
+
+
